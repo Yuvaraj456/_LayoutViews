3b6bd43ede5ae2ef00231affd22ffe4f95d376a9
Yuvaraj Nagarajan <yuvarajryd1@outlook.com>
2023-05-29 21:58:15 +0530

    29/05/2023 Learnings

---
+++ _LayoutViews/Controllers/ParcialController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _LayoutViews.Controllers
+{
+    public class ParcialController : Controller
+    {
+        [Route("/ParcialHome")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/ParcialAbout")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ _LayoutViews/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _LayoutViews.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("/Products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/Search")]
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [Route("/Order")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
